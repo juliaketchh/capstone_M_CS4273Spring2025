f4d952b9cc7b5353fb987e1f8e43aaacf7012cbf
Vincent <47754918+Chimp56@users.noreply.github.com>
2025-02-04 16:53:01 -0600

    1.5: implemented cohere service
    the cohere model can be used interchangably by the story controller

--- backend/src/service/cohere_service.py
+++ backend/src/service/cohere_service.py
@@ -1,2 +1,31 @@
+import cohere
+import dotenv
+from os.path import dirname, join
+
+backend_directory = dirname(dirname(dirname(__file__)))
+dotenv.load_dotenv(join(backend_directory, ".env"))
 class CohereService:
-    ...
+    def __init__(self):
+        self.co = cohere.ClientV2(api_key="8wo4mzxsPl5r51MrdCD5sLsQXBO470QkuHyYhn4s")
+
+    def generate_story(self, genre, perspective, tone, protagonist_name):
+        # Customize the prompt based on user input
+        prompt = f"Write a {genre} story in the {perspective} perspective with a {tone} tone, featuring a protagonist named {protagonist_name}."
+
+        # Query Cohere to generate the story
+        response = self.co.chat(model="command-r-plus-08-2024", messages=[{"role": "user", "content": prompt}])
+        story = response.message.content[0].text
+        return story
+    
+    def generate_random_story(self):
+        prompt = "Write a story that will surprise me"
+
+        # Query Cohere to generate the story
+        response = self.co.chat(model="command-r-plus-08-2024", messages=[{"role": "user", "content": prompt}])
+        story = response.message.content[0].text
+        return story
+    
+if __name__ == '__main__':
+    service = CohereService()
+    story = service.generate_random_story()
+    print(story)
