19277f9bc41467f358e4c1d121c85ce0af1c0d7e
Vincent <47754918+Chimp56@users.noreply.github.com>
2025-02-07 09:36:31 -0600

    1.5: created tests for story service
    Added return type to a couple function in story service. We should try to declare type when possible.

--- backend/src/service/story_service.py
+++ backend/src/service/story_service.py
@@ -4,13 +4,13 @@ class StoryService:
     def __init__(self):
         self.repository = StoryRepository()
 
-    def create_story(self, data):
+    def create_story(self, data) -> dict:
         return self.repository.create_story(data)
 
     def get_user_stories(self, user_id: int) -> list:
         return self.repository.get_stories_by_user_id(user_id)
 
-    def get_story(self, story_id):
+    def get_story(self, story_id:int) -> dict:
         return self.repository.get_story_by_id(story_id)
 
     def update_story(self, story_id, data):
