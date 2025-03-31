babd31bbc629bdf550eb469545e08c3daf708d87
Vincent <47754918+Chimp56@users.noreply.github.com>
2025-03-31 10:35:53 -0500

    3.9: started library page development
    I used mantine UI library. The library is broken down into two main components: Carousel and AllStories. Library makes an API call passing the stories as a prop to child components.
    
    I am still working on creating the carousel. I have created the AllSories component but I need to fix the styling. The useEffect for making the API call is currently commented out and I am using mock data.

--- frontend/bedtime_stories_webapp/src/main.jsx
+++ frontend/bedtime_stories_webapp/src/main.jsx
@@ -4,15 +4,23 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./pages/App";
 import Login from "./pages/Login";
 import "./index.css"; 
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  colorScheme: 'light',
+  primaryColor: 'blue',
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/app" element={<App />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/app" element={<App />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </BrowserRouter>
+      </MantineProvider>
   </StrictMode>
 );
