97351a17cc482f004335c9811bae4aa47994c435
Vincent <47754918+Chimp56@users.noreply.github.com>
2025-04-04 11:54:56 -0500

    3.9: created carousel

---
+++ frontend/bedtime_stories_webapp/src/components/Carousel.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1
+  }
+};
+
+export default function CustomCarousel({ items }) {
+  return (
+    <Carousel
+      swipeable={true}
+      draggable={true}
+      showDots={true}
+      responsive={responsive}
+      ssr={true}
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={2000}
+      keyBoardControl={true}
+      customTransition="all .5"
+      transitionDuration={500}
+      containerClass="carousel-container"
+      removeArrowOnDeviceType={["tablet", "mobile"]}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+    >
+      {items.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </Carousel>
+  );
+}
